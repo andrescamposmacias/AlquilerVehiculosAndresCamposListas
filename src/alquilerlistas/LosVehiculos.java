b5fae99bcc565f5b62cea32d3cf3504ea18ed3f8
Andres <andrescamposmacias@gmail.com>
2019-03-01 13:13:19 +0100

    Pedir al usuario el nif, la matricula y dias, y pasarle esos 3 atributos al metodo alquilerVehiculos para alquilar el vehiculo

--- src/alquilerlistas/LosVehiculos.java
+++ src/alquilerlistas/LosVehiculos.java
@@ -5,6 +5,8 @@
  */
 package alquilerlistas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author andres
@@ -12,6 +14,10 @@ package alquilerlistas;
 public class LosVehiculos {
     
     public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner (System.in);
+        String nif, matricula;
+        int alquiler;
         
         //Creo un objeto de EmpresaAlquiler
         EmpresaAlquiler listaAndres = new EmpresaAlquiler("Domotica", "Andrés", "www.domoticaandres.com");
@@ -24,6 +30,26 @@ public class LosVehiculos {
         listaAndres.imprimirClientes();
         listaAndres.imprimirVehiculos();
         
+        //pedimos el NIF al usuario
+        System.out.println("Introduce su NIF");
+        nif = teclado.nextLine();
+        
+        teclado.nextLine();//Limpiamos buffer
+        
+        //pedimos la matricula
+        System.out.println("Introduce la matricula del vehiculo");
+        matricula = teclado.nextLine();
+        
+        teclado.nextLine();//Limpiamos buffer
+        
+        //y por ultimo pedimos el numero de dia de alquiler
+        System.out.println("Introduce el número de dias de alquiler");
+        alquiler = teclado.nextInt();
+        
+        //ahora usamos el metododo alquilerVehiculo, que pasandole la matricula, el nif y los dias
+        //el cliente alquilara el vehiculo que pide el numero de dias que quiere si el vehiculo no esta
+        //alquilado
+        listaAndres.alquilarVehiculo(matricula, nif, alquiler);
     }
 
 }
